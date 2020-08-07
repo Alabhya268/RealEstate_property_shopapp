0f7ff5f65b737e8b8721172267f1bb2276bdb46e
Alabhya268 <singhalabhya32@gmail.com>
2020-08-08 03:28:39 +0530

    Initial Commit

---
+++ Realstste_property_shopapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Realstste_property_shopapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Realstste_property_shopapp.settings')
+
+application = get_asgi_application()
