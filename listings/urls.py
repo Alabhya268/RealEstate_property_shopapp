b5e6a19cd758ccacfae5675e16edae1dcb414d69
Alabhya268 <singhalabhya32@gmail.com>
2020-08-22 02:42:15 +0530

    Added Dynamic listing and paging in frontend

--- listings/urls.py
+++ listings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('<int:listing_id>', views.search, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search')
 ]
