c00d1b2af6218d12af22a91aad40438b032ddb25
Alabhya268 <singhalabhya32@gmail.com>
2020-09-02 04:03:42 +0530

    Added Lougout Feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 def register(request):
     if request.method == 'POST':
@@ -22,7 +24,7 @@ def register(request):
                     return redirect('register')
                 
                 else:
-                    user  = User.objects.create(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
+                    user = User.objects.create(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
                     # Loggin after register
                     # auth.login(request,user)
                     # messages.success(request,"You are now logged in")
@@ -39,15 +41,28 @@ def register(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        #Login users
-        return
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      messages.success(request, 'You are now logged in')
+      return redirect('dashboard')
     else:
-        return render(request, 'accounts/login.html')
+      messages.error(request, 'Invalid credentials')
+      return redirect('login')
+  else:
+    return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request,"You are now logged out")
+        return redirect('index')
 
 
 def dashboard(request):
