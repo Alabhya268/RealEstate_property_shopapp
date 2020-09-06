ca094255d84fdd2048bc453c67d62e3c4ce170ce
Alabhya268 <singhalabhya32@gmail.com>
2020-09-06 21:34:58 +0530

    Added Enquiry Form Functionality

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,11 +1,12 @@
+  
 from django.contrib import admin
 
-from .models import Contacts
- 
-class ContactsAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'name', 'listing', 'email', 'contact_date')
-    search_fields = ('id', 'name', 'listing' , 'email')
-    list_display_links = ('id', 'name')
-    list_per_page = 25 
-admin.site.register(Contacts, ContactsAdmin)    
+from .models import Contact
 
+class ContactAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name', 'email', 'listing')
+  list_per_page = 25
+
+admin.site.register(Contact, ContactAdmin)
